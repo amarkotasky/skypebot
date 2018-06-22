71b43ba9ccc838f03dd49a58443256b1ffa1420d
amar.khan <amar.khan@i-exceed.com>
2018-06-22 16:11:08 +0530

    all

---
+++ env.sh
@@ -0,0 +1,10 @@
+# export MicrosoftAppId='16b4b034-d3cf-457a-a139-3ffbae381990'
+# export MicrosoftAppPassword='srhRRYP895_?^lbvoUGC25@'
+
+export MicrosoftAppId='d175a9e2-340f-4471-a24e-9f63cfc9613b'
+export MicrosoftAppPassword='atcMZV6298(*}rstmLUBZ4|'
+
+
+export SKYPE_BOT_ID="Astro1"
+export MICROSOFT_APP_ID="16b4b034-d3cf-457a-a139-3ffbae381990"
+export MICROSOFT_APP_SECRET="srhRRYP895_?^lbvoUGC25@"
